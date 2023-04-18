cdce07acfc98a7374bb12ba2357bee28541ba963
Yaroslav Halchenko <debian@onerussian.com>
2023-04-18 12:53:46 -0400

    more robust remote handling etc

--- common.sh
+++ common.sh
@@ -10,12 +10,16 @@ if [ -z "${GITHUB_TOKEN:-}" ]; then
     GITHUB_TOKEN=$(git config hub.oauthtoken)
 fi
 
-if ! git remote | grep "$ghremote"; then
-   if ! GH_TOKEN="$GITHUB_TOKEN" gh repo fork --remote --remote-name "$ghremote"; then
-    echo "errored out, sleeping, trying to fetch"
-    sleep 2
-    git fetch "$ghremote"
-   fi
+if git fetch -v 2>&1 | grep -q github.com; then
+    if ! git remote | grep "$ghremote"; then
+       if ! GH_TOKEN="$GITHUB_TOKEN" gh repo fork --remote --remote-name "$ghremote"; then
+        echo "errored out, sleeping, trying to fetch"
+        sleep 2
+        git fetch "$ghremote"
+       fi
+    fi
+    mkdir -p .github/workflows
+else
+    echo "Remote does not point to github, fork youself"
 fi
-mkdir -p .github/workflows
 
